e5322e3ad6665e7645c50a0c72c5d2f6bd31f225
Camron Rule <camronrule@gmail.com>
2024-11-21 19:36:46 -0500

    done

--- src/main.hs
+++ src/main.hs
@@ -1,11 +1,34 @@
 -- Write printAMessage here
+printAMessage :: Show a => a -> IO ()
+printAMessage x = putStrLn (show x)
 
 -- Write division here
-
+division :: Double -> Double -> Maybe Double
+division x y
+    | y == 0 = Nothing
+    | otherwise = Just (x / y)
+    
 -- Write factorial here
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n-1)
 
 -- Write factList here
+factList :: Int -> [Int]
+factList n = [factorial x | x <- [0..n-1]]
 
 -- Write merge here
+merge :: [Int] -> [Int] -> [Int]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
+    | x <= y = x : merge xs (y:ys)
+    | otherwise = y : merge(x:xs) ys
 
-main = print "Test" -- Replace this with your testing code
+main :: IO ()
+main = do
+    printAMessage "TESTING"
+    print(division 4 2)
+    print(factorial 3)
+    print(factList 3)
+    print(merge [10,30,50,70] [0,20,40,60])
